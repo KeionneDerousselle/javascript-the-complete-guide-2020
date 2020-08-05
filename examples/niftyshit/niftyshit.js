1cbd7587e7db24344738902374d9dc24b809c5cb
Keionne Derousselle <knnderousselle@gmail.com>
2020-08-04 23:14:20 -0600

    notes on the javascript complete 2020 course

---
+++ examples/niftyshit/niftyshit.js
@@ -0,0 +1,13 @@
+// the + symbol in front of a string parses a number
+// it will parse it to the best of it's ability to either an integer if it doesn't have decimals, or a string if it does
+console.log(+`1` + +`2`)
+
+// if 1 + '1' = '11'....what is 1 * '1'
+console.log(3 + '3')
+console.log(3 * '3')
+
+// what does defer on a script tag mean?
+// what does async on a script tag mean?
+// how are they different?
+// do defer or async work on inline script tags?
+// what happens if you use the src attribute on an inline script tag?
